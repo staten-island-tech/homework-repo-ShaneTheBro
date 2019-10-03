6737eeb4ccf6de79ef380b04d7fff6abb5b2c23c
ShaneTheBro <50725203+ShaneTheBro@users.noreply.github.com>
2019-10-03 11:46:08 -0400

    Update app.js

--- hw6/app.js
+++ hw6/app.js
@@ -1,24 +1,35 @@
-const list = [  <li data-time="5:17">Flexbox Video</li>
-<li data-time="8:22">Flexbox Video</li>
-<li data-time="3:34">Redux Video</li>
-<li data-time="5:23">Flexbox Video</li>
-<li data-time="7:12">Flexbox Video</li>
-<li data-time="7:24">Redux Video</li>
-<li data-time="6:46">Flexbox Video</li>
-<li data-time="4:45">Flexbox Video</li>
-<li data-time="4:40">Flexbox Video</li>
-<li data-time="7:58">Redux Video</li>
-<li data-time="11:51">Flexbox Video</li>
-<li data-time="9:13">Flexbox Video</li>
-<li data-time="5:50">Flexbox Video</li>
-<li data-time="5:52">Redux Video</li>
-<li data-time="5:49">Flexbox Video</li>
-<li data-time="8:57">Flexbox Video</li>
-<li data-time="11:29">Flexbox Video</li>
-<li data-time="3:07">Flexbox Video</li>
-<li data-time="5:59">Redux Video</li>
-<li data-time="3:31">Flexbox Video</li>];
+const list = document.querySelector('li');
+
+
 
 
 let array = Array.from(list);
 console.log(array);
+
+console.log('Q1')
+const LIs = array.from(document.querySelectorAll('ul>li'));
+console.log(LIs);
+
+const LIsFiltered = LIs.filter(LI => LI.textContent == "Flexbox Video"); //arrow implies return
+console.log(LIsFiltered);
+
+const mappedLIs = LIsFiltered.map(obj => {
+    return obj.dataset.time;
+});
+
+console.log(mappedLIs);
+
+let sum = mappedSeconds.reduce((acc, val))
+
+const filteredChained = Array.from(document.querySelectorAll('ul>li'))
+.filter(LI => LI.textContent == "Flexbox Video")
+.map(obj => {return obj.dataset.time;}) //need return because of curly brackets
+.map(timeCode => {
+       const parts = timeCode.split(':');
+    //console.log(parts);
+    return (parseInt(parts[0] * 60) + parseInt(parts[1]));
+})
+
+.reduce((acc, val) => {
+    return acc + val;
+}) //all it means is the stuff is added together
